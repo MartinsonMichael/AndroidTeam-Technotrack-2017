ef036a0d66e959bf2b0f46666d90613d64c7090f
EgorovMike219 <mike219@mail.ru>
2017-10-25 18:39:25 +0300

    check Internet connection

--- app/src/main/java/com/technothack/michael/music/AuthActivity.java
+++ app/src/main/java/com/technothack/michael/music/AuthActivity.java
@@ -1,6 +1,9 @@
 package com.technothack.michael.music;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -19,6 +22,7 @@ public class AuthActivity extends AppCompatActivity {
     EditText phoneNumber, code;
     Button send_auth;
     int step; // 0 - ввод номера телефона, 1 - ввод кода
+    TdApi.TLObject result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,34 +41,55 @@ public class AuthActivity extends AppCompatActivity {
         phoneNumber = (EditText) findViewById(R.id.phoneNumber);
         code = (EditText) findViewById(R.id.code);
         send_auth = (Button) findViewById(R.id.send_auth);
-        //TODO: проверить подключение к интернету
-        send_auth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (step == 0 && phoneNumber.getText().toString().length() != 0) {
-                    client.sendPhone(phoneNumber.getText().toString(), new Client.ResultHandler() {
-                        @Override
-                        public void onResult(TdApi.TLObject object) {
-                            text_auth.setText(object.toString());
+        // проверить подключение к интернету
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            send_auth.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (step == 0 && phoneNumber.getText().toString().length() != 0) {
+                        client.sendPhone(phoneNumber.getText().toString(), new Client.ResultHandler() {
+                            @Override
+                            public void onResult(TdApi.TLObject object) {
+                                text_auth.setText(object.toString());
+                            }
+                        });
+                        client.getAuthState(new Client.ResultHandler() {
+                            @Override
+                            public void onResult(TdApi.TLObject object) {
+                                result = object;
+                            }
+                        });
+                        if (result.getConstructor() != new TdApi.AuthStateWaitCode().getConstructor()) {
+                            step = 1; // это нужно делать только в случае если не возникло ошибки
+                            code.setVisibility(View.VISIBLE);
                         }
-                    });
-                    step = 1; // это нужно делать только в случае если не возникло ошибки
-                    code.setVisibility(View.VISIBLE);
-                }
-                if (step == 1 && code.getText().toString().length() != 0) {
-                    // все что написано дальше должно выполняться только в случае успешной авторизации
-                    client.checkCode(code.getText().toString(), "", "", new Client.ResultHandler() {
-                        @Override
-                        public void onResult(TdApi.TLObject object) {
-                            text_auth.setText(object.toString());
+                    }
+                    if (step == 1 && code.getText().toString().length() != 0) {
+                        client.checkCode(code.getText().toString(), "", "", new Client.ResultHandler() {
+                            @Override
+                            public void onResult(TdApi.TLObject object) {
+                                text_auth.setText(object.toString());
+                            }
+                        });
+                        //в случае успешной авторизации
+                        client.getAuthState(new Client.ResultHandler() {
+                            @Override
+                            public void onResult(TdApi.TLObject object) {
+                                result = object;
+                            }
+                        });
+                        if (result.getConstructor() != new TdApi.AuthStateOk().getConstructor()) {
+                            Intent intent = new Intent(AuthActivity.this, StartingActivity.class);
+                            startActivity(intent);
+                            AuthActivity.this.finish();
                         }
-                    });
-                    //в случае успешной авторизации
-                    Intent intent = new Intent(AuthActivity.this, StartingActivity.class);
-                    startActivity(intent);
-                    AuthActivity.this.finish();
+                    }
                 }
-            }
-        });
+            });
+        } else {
+            text_auth.setText(R.string.network_state_0);
+        }
     }
 }
